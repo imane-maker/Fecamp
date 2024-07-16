7cb0a0373af9e5d1d02b2ac53715dd000d8ada51
imane-maker <faskaimane@gmail.com>
2024-07-16 17:24:14 +0200

    Add files via upload

---
+++ python/python.py
@@ -0,0 +1,72 @@
+print("Hello welcome to the Algo bot")
+menu = '''Pick one option(off-to finish)'''
+
+1-Music
+2-Games
+3-Movies 
+4-Sports
+
+choice = input(menu)
+while choice != "off":
+    #Music 
+    if choice == "1" or choice.lower() == Music
+    genre = input("What type of music do you like?")
+    if genre == "french":
+        print("formidable")
+    if genre == "english":
+        print("heart wave")
+    if genre == "spanish":
+        print("despacito")
+    if genre == "arabic":
+        print("africain")
+    else :
+        print("mexicain")
+
+    #Games
+    if choice == "2" or choice.lower() == Games
+    genre = input("What type of games do you like?")
+    if genre == "family":
+        print("Monopoly")
+    if genre == "adventure":
+        print("The legend of Zelda")
+    if genre == "strategy":
+        print("Civilization")
+    if genre == "horror":
+        print("Resident Evil")
+    else :
+        print("Candy crush")
+
+    #Movies
+    if choice == "3" or choice.lower() == Movies
+    genre = input("What type of movies do you like?")
+    if genre == "action":
+        print("fast and furious6")
+    if genre == "horror":
+        print("it")
+    if genre == "historical drama":
+        print("gladiator")
+    if genre == "Sport drama":
+        print("Creed")
+    else :
+        print("Lupin")
+
+    #Sport
+    if choice == "4" or choice.lower() == sport
+    genre = input("What type of sport do you like ? ")
+    if genre == "team sports":
+        print("football") 
+    if genre == "water sports":
+        print("surf")
+    if genre == "combats sports":
+        print("judo")
+    if genre == "racquet sports":
+        print("tennis")
+    else :
+        print("fornite")
+
+
+
+              
+    
+    
+    
